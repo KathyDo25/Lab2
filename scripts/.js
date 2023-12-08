5ec1853fb29c8fc0c25d37c3dff05c5516748c48
KathyDo25 <147874108+KathyDo25@users.noreply.github.com>
2023-12-08 02:57:30 -0700

    update: whole js

---
+++ scripts/.js
@@ -0,0 +1,48 @@
+// Footer - Display current year
+const currentYear = new Date().getFullYear();
+document.getElementById('').innerHTML=currentYear;
+
+//Allert Button
+document.getElementById('btn-alert').addEventListener('click',function() {
+    alert('Hi everyone!');
+});
+
+// Hover Button
+const hoverButton = document.getElementById('btn-alert');
+
+hoverButton.onmouseover = ()=>{
+    hoverButton.innerText='Click Me!';
+};
+hoverButton.onmouseleave = () => {
+    hoverButton.innerText='Hover me!';
+};
+
+// Incremensing Button
+let count = 1;
+const counterButton = document.getElementById('btn-counter');
+const counterText = document.getElementById('txt-counter');
+
+counterButton.onclick = () => {
+    count = count + 1;
+    counterText.innerHTML='Number:${count}';
+    // If-else statement
+    if (count % 2 === 0){
+        counterText.classList.remove('odd');
+        counterText.classList.add ('even');
+    }
+    else{
+        counterText.classList.remove('even');
+        counterText.classList.add('odd');
+    }
+};
+
+// for Loops
+const numberList = document.getElementById('numbers');
+const initialValue=1;
+const stopValue=100;
+
+for (let i=initialValue; i<=stopValue; i++) {
+    const listItem=document.createElement('li');
+    listItem.textContent=i%2 === 0? 'even':'odd';
+    numbersList.appendChild(listItem);
+}
